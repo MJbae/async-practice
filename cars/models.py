11e0863cd6cb9676f61dea510e2ac92e96a89ea3
MJbae <bmj5208@gmail.com>
2021-12-28 09:28:29 +0900

    feat: add relationship in models

--- cars/models.py
+++ cars/models.py
@@ -2,15 +2,6 @@ import string
 from django.db import models
 
 
-class Car(models.Model):
-    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
-    code = models.CharField(max_length=3, null=False, blank=False, unique=True)
-    symbol = models.CharField(max_length=5, null=False, blank=False, default='$')
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class Owner(models.Model):
     name = models.CharField(max_length=64, null=True)
 
@@ -20,6 +11,17 @@ class Owner(models.Model):
 
 class Customer(models.Model):
     name = models.CharField(max_length=64, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Car(models.Model):
+    owners = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True)
+    customers = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=120, null=False, blank=False, unique=True)
+    code = models.CharField(max_length=3, null=False, blank=False, unique=True)
+    symbol = models.CharField(max_length=5, null=False, blank=False, default='$')
 
     def __str__(self) -> str:
         return self.name
