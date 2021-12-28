086978123ed55c2f21e286c62a79910b1519183c
MJbae <bmj5208@gmail.com>
2021-12-28 09:53:00 +0900

    feat: add celery-rabbitmq setting

---
+++ config/celery.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from celery import Celery
+
+app = Celery('config',
+             broker='amqp://maxwell:passwordOfmaxwell123@localhost/maxwell_vhost',
+             backend='rpc://',
+             include=['tasks'])
