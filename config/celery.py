a52dcf5e9e1677fa81b49ffdc81b18bd0648a0d9
MJbae <bmj5208@gmail.com>
2021-12-28 18:40:29 +0900

    feat: allow to refer objects in django objects(such as models) in tasks

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import
 from celery import Celery
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery('config',
              broker='redis://localhost:6379/1',
              backend='rpc://',
              include=['cars.tasks'])
+
