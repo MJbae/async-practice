931eaa55b0399b73c72cd9255aada565f956ce4e
MJbae <bmj5208@gmail.com>
2022-01-06 14:19:36 +0900

    feat: set up result backend for sqs

--- config/celery.py
+++ config/celery.py
@@ -7,6 +7,6 @@ from config.settings import CELERY_BROKER_URL
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery('config',
              broker=CELERY_BROKER_URL,
-             backend=None,
+             backend='django-db',
              include=['cars.tasks'])
 
