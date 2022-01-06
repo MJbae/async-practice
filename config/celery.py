b6bb687565c87268ee8633abce1714ace75aeff0
MJbae <bmj5208@gmail.com>
2022-01-06 17:15:47 +0900

    feat: set time setting for celery beat

--- config/celery.py
+++ config/celery.py
@@ -8,3 +8,12 @@ app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'add-every-monday-morning': {
+        'task': 'cars.tasks',
+        'schedule': crontab(hour=7, minute=30, day_of_week=1),
+        'args': (16, 16),
+    },
+}
