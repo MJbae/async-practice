aff9231d695e69079c39ccc6f58cc863883ad040
MJbae <bmj5208@gmail.com>
2022-01-06 15:05:11 +0900

    clean: Remove useless importing package

--- config/celery.py
+++ config/celery.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 import os
 
-from config.settings import CELERY_BROKER_URL
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
 
