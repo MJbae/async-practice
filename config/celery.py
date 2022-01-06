f1f4c5f76f9b2c468f2ae4858424561b669a13e4
MJbae <bmj5208@gmail.com>
2022-01-06 12:17:00 +0900

    clean: remove useless code

--- config/celery.py
+++ config/celery.py
@@ -5,7 +5,6 @@ import os
 from config.settings import CELERY_BROKER_URL
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-print(f'CELERY_BROKER_URL: {CELERY_BROKER_URL}')
 app = Celery('config',
              broker=CELERY_BROKER_URL,
              backend=None,
